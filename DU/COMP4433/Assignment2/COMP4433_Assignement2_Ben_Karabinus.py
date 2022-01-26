9a4016e76121ce2d51758e629c7442e8814cff5e
Ben Karabinus <benjaminkarabinus@gmail.com>
2022-01-26 04:59:51 -0700

    COMP 4433 Assignment 2

--- DU/COMP4433/Assignment2/COMP4433_Assignement2_Ben_Karabinus.py
+++ DU/COMP4433/Assignment2/COMP4433_Assignement2_Ben_Karabinus.py
@@ -1,6 +1,10 @@
-from cProfile import label
-from ctypes import alignment
-from turtle import width
+"""
+Ben Karabinus
+University of Denver
+COMP 4433, Winter Quarter 2022
+Assignmnt 2
+"""
+from shutil import which
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -9,9 +13,9 @@ import yfinance as yf
 
 def main():
 
-    #partOne()
+    partOne()
     #partTwo()
-    partThree()
+    #partThree()
     #partFour()
 
 
@@ -19,6 +23,7 @@ def partOne():
     weights = np.rint(np.array([1.08, 4.003, 6.940, 9.012, 10.810, 12.001]))
     weights = weights.astype(int)
     title = "Atomic Weights"
+    title2 = "Atomic Weights (Pct of Whole)"
     labels = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon"]
     explodeOne = (0, 0, 0, 0, 0, 0.1)
     explodeTwo = (0, 0.1, 0, 0, 0, 0)
@@ -27,7 +32,7 @@ def partOne():
     ax[0].pie(weights, labels=labels, explode=explodeOne,
               autopct=lambda p: '{:.0f}'.format(p*sum(weights)/100))
     ax[0].axis('equal')  # no ovular pie charts
-    ax[1].set_title(title)
+    ax[1].set_title(title2)
     ax[1].pie(weights, labels=labels, explode=explodeTwo,
               autopct='%1.1f%%')
     ax[1].axis('equal')  # no ovular pie charts
@@ -54,26 +59,22 @@ def partTwo():
 
 def partThree():
 
-
-    dates = ['2021-01-10', '2021-02-10', '2021-03-10', '2021-04-10',
-             '2021-05-10', '2021-06-10', '2021-07-10', '2021-08-10']
-    nums = np.random.uniform(100, 200, 8)
+    dates = ['2021-01-01', '2021-01-02', '2021-01-03', '2021-01-04',
+             '2021-01-05', '2021-01-06', '2021-01-07', '2021-01-08']
+    nums = np.random.uniform(100, 201, 8)
     df = pd.DataFrame({'date': dates, 'nums': nums})
     df['date'] = pd.to_datetime(df['date'])
     df = df.set_index('date')
-    print(df)
-    fig, ax = plt.subplots(1,2, figsize=(16, 8))
-    """
-    
+    fig = plt.figure(figsize=(16, 8))
     ax0 = fig.add_axes([0.15, 0.25, 0.3, 0.4])
     ax0.plot(df['nums'])
+    ax0.grid(which='major', alpha=0.25)
     plt.xticks(rotation=65)
     ax1 = fig.add_axes([0.525, 0.25, 0.3, 0.4])
-    ax1.barh(df.index, df['nums'], align='center')
+    ax1.bar(df.index, df['nums'])
     plt.xticks(rotation=65)
-    """
-    ax[0].plot(df['nums'])
-    ax[1].bar(pd.date_range(start=df.index[0], end=df.index[7], periods=8), df['nums'], width=10, align='center')
+    ax0.set_title("Dates vs Numbers (Line)")
+    ax1.set_title("Dates vs Numbers (Bar)")
     plt.tight_layout()
     plt.show()
 
@@ -82,18 +83,17 @@ def partFour():
 
     googleJan21 = yf.download('GOOGL', '2021-01-01', '2021-01-31')
     fig = plt.figure(figsize=(16, 8))
-    ax0 = fig.add_axes([0.1, 0.5, 0.8, 0.3])
+    fig.align_xlabels()
+    ax0 = fig.add_axes([0.1, 0.55, 0.8, 0.3])
     ax0.plot(googleJan21['High'], 'g', label='High')
     ax0.plot(googleJan21['Low'], 'r', label='Low')
     ax0.plot(googleJan21['Open'], 'b', label='Open')
-    plt.legend(loc=0)
+    ax0.set_title("Google Stock Prices January 2021 (USD)")
+    plt.legend()
     ax1 = fig.add_axes([0.1, 0.1, 0.8, 0.3])
     ax1.plot(googleJan21.index, googleJan21['Volume'])
-    #plt.legend()
-    plt.tight_layout()
+    ax1.set_title("Google Stock Volume January 2021 (millions of shares)")
     plt.show()
-    
-
 
 
 if __name__ == '__main__':
