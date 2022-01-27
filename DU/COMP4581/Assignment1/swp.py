63a20c96ba020d5de410e3c364bd09f8c2ac4fe8
Ben Karabinus <benjaminkarabinus@gmail.com>
2022-01-27 06:05:27 -0700

    COMP 4581 Assignment 1

---
+++ DU/COMP4581/Assignment1/swp.py
@@ -0,0 +1,100 @@
+"""
+Ben Karabinus
+University of Denver
+COMP 4581, Winter quarter 2022
+Assignment 1
+
+"""
+from collections import defaultdict
+
+
+class MyQueue:
+
+    def __init__(self):
+        self.q = []
+
+    def qSize(self):
+        return len(self.q)
+
+    def enqueue(self, item):
+        self.q.append(item)
+
+    def dequeue(self):
+        return self.q.pop(0)
+
+    def empty(self):
+        if len(self.q) == 0:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+
+        strQ = " ".join(self.q)
+        return strQ
+
+
+def loadGraph(edgeFileName):
+
+    with open(edgeFileName, 'r') as f:
+        edges = [line.split() for line in f.readlines()]
+    f.close()
+
+    adjList = defaultdict(list)
+    for edge in edges:
+        u, v = edge[0], edge[1]
+        adjList[u].append(v)
+        adjList[v].append(u)
+
+    return adjList
+
+
+def BFS(G, s):
+
+    dist = {}
+    q = MyQueue()
+    vertices = G.keys()
+
+    for u in vertices:  # O(n)
+        dist[u] = float('inf')
+    dist[s] = 0
+    q.enqueue(s)
+    while not q.empty():
+        u = q.dequeue()
+        neighbors = G[u]
+        for neighbor in neighbors:  # O(n+m)
+            if dist[neighbor] == float('inf'):
+                dist[neighbor] = dist[u]+1
+                q.enqueue(neighbor)
+
+    return dist
+
+
+def distanceDistribution(G):
+
+    distribution = defaultdict(int)
+    numDistances = 0
+
+    for v in G.keys():
+        dist = BFS(G, v)
+        for d in dist.values():
+            distribution[d] += 1
+
+    numDistances = sum(distribution.values())
+    distribution = {key: "{:.2%}".format(value / numDistances) for
+                    key, value in distribution.items()}
+
+    return distribution
+
+
+def main():
+
+    adjList = loadGraph('/Users/benkarabinus/Documents/Git/nerdy_data_stuff/'
+                        'DU/COMP4581/Assignment1/edges.txt')
+    distribution  = distanceDistribution(adjList)
+    print(distribution)
+
+
+if __name__ == '__main__':
+    main()
+
