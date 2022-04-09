fb33557426155b754885cfb32e9c5f5f11d6f6a5
Ben Karabinus <benjaminkarabinus@gmail.com>
2022-04-09 10:34:10 -0600

    Commit COMP 4447 Asynch week 3

---
+++ DU/COMP4447/Week3/hello.sh
@@ -0,0 +1,7 @@
+#!/bin/bash shell to use to tun this file
+# first postional parameter
+first_name=$1
+# second postional parameter
+last_name=$2
+echo "Hello, $first_name, $last_name"
+echo "Thank you for running $0"
