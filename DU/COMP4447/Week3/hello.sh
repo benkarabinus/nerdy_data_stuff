b11875a4cb388d07c9f417780bcf115fe771637c
Ben Karabinus <benjaminkarabinus@gmail.com>
2022-04-12 19:25:35 -0600

    COMP 4431 Assignment 3

--- DU/COMP4447/Week3/hello.sh
+++ DU/COMP4447/Week3/hello.sh
@@ -5,3 +5,9 @@ first_name=$1
 last_name=$2
 echo "Hello, $first_name, $last_name"
 echo "Thank you for running $0"
+echo "Now lets count a little with a for loop"
+for i in $(seq 5)
+do
+echo $first_name
+echo $iteration $i
+done
